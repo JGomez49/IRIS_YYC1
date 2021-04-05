ab75fc397e3ffedbe52b1a89f4934d7a7d4a738c
John Gonzalez <47705426+JGomez49@users.noreply.github.com>
2021-04-05 09:01:43 -0600

    cambio de puerto

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cowsay = require('cowsay');
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
 app.set('views' , __dirname + '/views');
